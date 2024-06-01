0cd5566fe5fa5aee50bfcd2c4ca6613b07f07f80
Giblert Brandow <simon@0100.se>
2024-06-01 22:02:05 +0200

    Created routes for authentication, #1

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bankid_authentication project.
+WSGI config for project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bankid_authentication.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
