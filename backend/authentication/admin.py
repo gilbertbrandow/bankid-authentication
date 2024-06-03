ae334ede430063af25354c66c43771bc9a73b330
Giblert Brandow <simon@0100.se>
2024-06-03 20:43:45 +0200

    Registered group in admin, #5

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin as BaseGroupAdmin
+from .models import User, Group
+class GroupAdmin(BaseGroupAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
 
+admin.site.register(Group, GroupAdmin)
 class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_superuser')
     list_filter = ('is_superuser', 'groups', 'user_permissions')
