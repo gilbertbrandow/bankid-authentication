ea521928395913e58babdda0b0b4235f39330816
Giblert Brandow <simon@0100.se>
2024-06-06 22:19:32 +0200

    Updated models with permission (#7)

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -2,25 +2,51 @@ import bcrypt
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.contrib.contenttypes.models import ContentType
 
 class AccountManager(models.Manager):
-    def create_account(self, title: str, **extra_fields)->'Account':
-        if not title: 
-            raise ValueError('The title field must be set')
-        title = title.strip()
-        account = self.model(title=title, **extra_fields)
+    def create_account(self, name: str, **extra_fields)->'Account':
+        if not name: 
+            raise ValueError('The name field must be set')
+        name = name.strip()
+        account = self.model(name=name, **extra_fields)
         account.save(using=self._db)
         return account
 
 class Account(models.Model):
-    title = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = AccountManager()
     
+    class Meta:
+        db_table = 'authentication_accounts'
+        
     def __str__(self)->str:
-        return str({"type": "account", "id": self.id, "title": self.title})
+        return str({"content_type": "account", "id": self.id, "name": self.name})
 
+class PermissionManager(models.Manager):
+    def create_permission(self, name: str, **extra_fields)->'Permission':
+        if not name: 
+            raise ValueError('The name field must be set')
+        name = name.strip()
+        permission = self.model(name=name, **extra_fields)
+        permission.save(using=self._db)
+        return permission
+
+class Permission(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    codename = models.CharField(max_length=100, unique=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='permissions')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = PermissionManager()
+    class Meta:
+        db_table = 'authentication_permissions'
+        
+    def __str__(self)->str:
+        return str({"content_type": "permission", "id": self.id, "name": self.name})
 
 class UserManager(models.Manager):
     def create_user(self, email, password=None, **extra_fields)->'User':
@@ -40,6 +66,7 @@ class UserManager(models.Manager):
 
 class User(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='users')
+    permissions = models.ManyToManyField(Permission, related_name='users', db_table='authentication_user_permission')
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=30, blank=True)
@@ -76,27 +103,33 @@ class User(models.Model):
         if self.pk is None and self.password:
             self.set_password(self.password)
         super().save(*args, **kwargs)
-
+        
+    class Meta:
+        db_table = 'authentication_users'
+        
     def __str__(self)->str:
-        return str({"type": "user", "id": self.id, "email": self.email})
-
+        return str({"content_type": "user", "id": self.id, "email": self.email})
 
 class GroupManager(models.Manager):
-    def create_Group(self, title: str, **extra_fields)->'Group':
-        if not title: 
-            raise ValueError('The title field must be set')
-        title = title.strip()
-        Group = self.model(title=title, **extra_fields)
+    def create_Group(self, name: str, **extra_fields)->'Group':
+        if not name: 
+            raise ValueError('The name field must be set')
+        name = name.strip()
+        Group = self.model(name=name, **extra_fields)
         Group.save(using=self._db)
         return Group
 
 class Group(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='users')
-    title = models.CharField(max_length=100, unique=True)
-    users = models.ManyToManyField(User, related_name='groups')
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='groups')
+    name = models.CharField(max_length=100, unique=True)
+    users = models.ManyToManyField(User, related_name='groups', db_table='authentication_group_user')
+    permissions = models.ManyToManyField(Permission, related_name='groups',  db_table='authentication_group_permission')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = GroupManager()
     
+    class Meta:
+        db_table = 'authentication_groups'
+        
     def __str__(self)->str:
-        return str({"type": "Group", "id": self.id, "title": self.title})
+        return str({"content_type": "group", "id": self.id, "name": self.name})
