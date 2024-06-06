30af4dfbf3fdecc92a7dbf810968f9a67ea81e5f
Giblert Brandow <simon@0100.se>
2024-06-06 22:54:54 +0200

    Added serializers, (#7)

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import User
+from .models import Account, Group, Permission, User
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
         fields = '__all__'
