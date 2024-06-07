8a98ba9d9df8ec95dc4f1e683de6e6d779fbb5ef
Giblert Brandow <simon@0100.se>
2024-06-07 13:31:34 +0200

    Added urls & views for accounts (#9)

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -4,8 +4,9 @@ URL configuration for authentication.
 """
 
 from django.urls import path
-from .views import UserList
+from .views import UserList, AccountList
 
 urlpatterns = [
     path('users', UserList.as_view(), name='users'),
+    path('accounts', AccountList.as_view(), name='accounts'),
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,8 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Account
+from .serializers import UserSerializer, AccountSerializer
+
+class AccountList(APIView):
+    def get(self, request):
+        Accounts = Account.objects.all()
+        serializer = AccountSerializer(Accounts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserList(APIView):
     def get(self, request):
