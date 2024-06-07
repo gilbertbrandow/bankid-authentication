08b49e272974c4f20306e9538b7cf497fd14e185
Giblert Brandow <simon@0100.se>
2024-06-07 16:14:38 +0200

    Endpoint for obtaining JWT token, (#8)

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -4,9 +4,10 @@ URL configuration for authentication.
 """
 
 from django.urls import path
-from .views import UserList, AccountList
+from .views import UserList, AccountList, ObtainJWTToken
 
 urlpatterns = [
+    path('token', ObtainJWTToken.as_view(), name='token_obtain'),
     path('users', UserList.as_view(), name='users'),
     path('accounts', AccountList.as_view(), name='accounts'),
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,10 +1,34 @@
+import jwt
+from django.conf import settings
+from datetime import datetime, timedelta
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User, Account
 from .serializers import UserSerializer, AccountSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import User
+
+class ObtainJWTToken(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = User.objects.authenticate(email=email, password=password)
+        
+        if user is not None:
+            payload = {
+                'user_id': user.id,
+                'exp': datetime.utcnow() + timedelta(hours=24),
+                'iat': datetime.utcnow(), 
+            }
+            token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+            return Response({'token': token}, status=status.HTTP_200_OK)
+        
+        return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class AccountList(APIView):
+    permission_classes = []
+
     def get(self, request):
         Accounts = Account.objects.all()
         serializer = AccountSerializer(Accounts, many=True)
@@ -18,6 +42,8 @@ class AccountList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserList(APIView):
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
