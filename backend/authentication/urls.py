81b22d0d97ad85b28007d104499094df1fe43c56
Giblert Brandow <simon@0100.se>
2024-06-09 20:01:05 +0200

    CRUD for permissions (closes #10)

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -4,14 +4,18 @@ URL configuration for authentication.
 """
 
 from django.urls import path
-from .views import AccountList, AccountDetail, GroupList, GroupDetail, UserList, UserDetail, ObtainJWTToken
+from .views import AccountList, AccountDetail, GroupList, GroupDetail, UserList, UserDetail, PermissionList, PermissionDetail, ObtainJWTToken
 
 urlpatterns = [
     path('token/', ObtainJWTToken.as_view(), name='token_obtain'),
+    
+    # Authentication app CRUD
     path('accounts/', AccountList.as_view(), name='account_list'),
     path('accounts/<int:pk>/', AccountDetail.as_view(), name='account_detail'),
     path('groups/', GroupList.as_view(), name='group_list'),
     path('groups/<int:pk>/', GroupDetail.as_view(), name='group_detail'),
     path('users/', UserList.as_view(), name='user_list'),
     path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
+    path('permissions/', PermissionList.as_view(), name='permission_list'),
+    path('permissions/<int:pk>/', PermissionDetail.as_view(), name='permission_detail'),
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -2,39 +2,46 @@ from django.http import HttpRequest
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import User, Account, Group
-from .serializers import UserSerializer, AccountSerializer, GroupSerializer
+from .models import User, Account, Group, Permission
+from .serializers import UserSerializer, AccountSerializer, GroupSerializer, PermissionSerializer
 from rest_framework.permissions import IsAuthenticated
 from .jwt_authentication import generate_jwt
+
+
 class ObtainJWTToken(APIView):
     permission_classes = []
-    
-    def post(self, request: HttpRequest) -> Response:
+
+    @staticmethod
+    def post(request: HttpRequest) -> Response:
         email = request.data.get('email')
         password = request.data.get('password')
         user = User.objects.authenticate(email=email, password=password)
-        
+
         if user is None:
             return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-        
+
         return Response({'token': generate_jwt(user)}, status=status.HTTP_200_OK)
-        
+
+
 class AccountList(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request: HttpRequest) -> Response:
+    @staticmethod
+    def get(request: HttpRequest) -> Response:
         Accounts = Account.objects.all()
         serializer = AccountSerializer(Accounts, many=True)
         return Response(serializer.data)
 
-    def post(self, request: HttpRequest) -> Response:
+    @staticmethod
+    def post(request: HttpRequest) -> Response:
         serializer = AccountSerializer(data=request.data)
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+
+
 class AccountDetail(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -47,68 +54,71 @@ class AccountDetail(APIView):
 
     def get(self, request: HttpRequest, pk: int) -> Account:
         account = self.get_object(pk)
-        
+
         if account is None:
             return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serializer = AccountSerializer(account)
         return Response(serializer.data)
 
     def put(self, request: HttpRequest, pk: int) -> Account:
         account = self.get_object(pk)
-        
+
         if account is None:
             return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serializer = AccountSerializer(account, data=request.data)
-        
+
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data)
 
     def patch(self, request: HttpRequest, pk: int) -> Account:
         account = self.get_object(pk)
-        
+
         if account is None:
             return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = AccountSerializer(account, data=request.data, partial=True)
+
+        serializer = AccountSerializer(
+            account, data=request.data, partial=True)
 
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data)
-        
+
     def delete(self, request: HttpRequest, pk: int) -> Account:
         account = self.get_object(pk)
-        
+
         if account is None:
             return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         account.delete()
         return Response({'success': 'Account deleted.'}, status=status.HTTP_204_NO_CONTENT)
 
 
 class GroupList(APIView):
     permission_classes = [IsAuthenticated]
-    
-    def get(self, request: HttpRequest) -> Response:
+
+    @staticmethod
+    def get(request: HttpRequest) -> Response:
         Groups = Group.objects.all()
         serializer = GroupSerializer(Groups, many=True)
         return Response(serializer.data)
 
-    def post(self, request: HttpRequest) -> Response:
+    @staticmethod
+    def post(request: HttpRequest) -> Response:
         serializer = GroupSerializer(data=request.data)
-        
+
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+
 
 class GroupDetail(APIView):
     permission_classes = [IsAuthenticated]
@@ -122,67 +132,71 @@ class GroupDetail(APIView):
 
     def get(self, request: HttpRequest, pk: int) -> Group:
         group = self.get_object(pk)
-        
+
         if group is None:
             return Response({'detail': 'Group not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serializer = GroupSerializer(group)
         return Response(serializer.data)
 
     def put(self, request: HttpRequest, pk: int) -> Group:
         group = self.get_object(pk)
-        
+
         if group is None:
             return Response({'detail': 'Group not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serializer = GroupSerializer(group, data=request.data)
-        
+
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data)
 
     def patch(self, request: HttpRequest, pk: int) -> Group:
         group = self.get_object(pk)
-        
+
         if group is None:
             return Response({'detail': 'Group not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serializer = GroupSerializer(group, data=request.data, partial=True)
 
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data)
-        
+
     def delete(self, request: HttpRequest, pk: int) -> Group:
         group = self.get_object(pk)
-        
+
         if group is None:
             return Response({'detail': 'Group not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         group.delete()
         return Response({'success': 'Group deleted.'}, status=status.HTTP_204_NO_CONTENT)
-    
+
+
 class UserList(APIView):
     permission_classes = [IsAuthenticated]
-    
-    def get(self, request: HttpRequest) -> Response:
+
+    @staticmethod
+    def get(request: HttpRequest) -> Response:
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
-    def post(self, request: HttpRequest) -> Response:
+    @staticmethod
+    def post(request: HttpRequest) -> Response:
         serializer = UserSerializer(data=request.data)
-        
+
         if serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+
+
 class UserDetail(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -195,47 +209,122 @@ class UserDetail(APIView):
 
     def get(self, request: HttpRequest, pk: int) -> User:
         user = self.get_object(pk)
-        
+
         if user is None:
             return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
     def put(self, request: HttpRequest, pk: int) -> User:
         user = self.get_object(pk)
-        
+
         if user is None:
             return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serializer = UserSerializer(user, data=request.data)
-        
+
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data)
 
     def patch(self, request: HttpRequest, pk: int) -> User:
         user = self.get_object(pk)
-        
+
         if user is None:
             return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         serializer = UserSerializer(user, data=request.data, partial=True)
 
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         serializer.save()
         return Response(serializer.data)
-        
+
     def delete(self, request: HttpRequest, pk: int) -> User:
         user = self.get_object(pk)
-        
+
         if user is None:
             return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         user.delete()
         return Response({'success': 'User deleted.'}, status=status.HTTP_204_NO_CONTENT)
-    
+
+
+class PermissionList(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def get(request: HttpRequest) -> Response:
+        Permissions = Permission.objects.all()
+        serializer = PermissionSerializer(Permissions, many=True)
+        return Response(serializer.data)
+
+    @staticmethod
+    def post(request: HttpRequest) -> Response:
+        serializer = PermissionSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class PermissionDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def get_object(pk: int) -> Permission | None:
+        try:
+            return Permission.objects.get(pk=pk)
+        except Permission.DoesNotExist:
+            return None
+
+    def get(self, request: HttpRequest, pk: int) -> Permission:
+        permission = self.get_object(pk)
+
+        if permission is None:
+            return Response({'detail': 'Permission not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PermissionSerializer(permission)
+        return Response(serializer.data)
+
+    def put(self, request: HttpRequest, pk: int) -> Permission:
+        permission = self.get_object(pk)
+
+        if permission is None:
+            return Response({'detail': 'Permission not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PermissionSerializer(permission, data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        return Response(serializer.data)
+
+    def patch(self, request: HttpRequest, pk: int) -> Permission:
+        permission = self.get_object(pk)
+
+        if permission is None:
+            return Response({'detail': 'Permission not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PermissionSerializer(
+            permission, data=request.data, partial=True)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request: HttpRequest, pk: int) -> Permission:
+        permission = self.get_object(pk)
+
+        if permission is None:
+            return Response({'detail': 'Permission not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        permission.delete()
+        return Response({'success': 'Permission deleted.'}, status=status.HTTP_204_NO_CONTENT)
