15c08a6113a0c4ecad47c64d3a2ad47da213ef33
Giblert Brandow <simon@0100.se>
2024-06-10 21:21:22 +0200

    Added new permissions class for same account resource (#10)

--- backend/authentication/permissions.py
+++ backend/authentication/permissions.py
@@ -1,17 +1,21 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
 from typing import Type, TYPE_CHECKING
+from authentication.models import Account
+from django.db import models
 
 if TYPE_CHECKING:
     from rest_framework.views import APIView
 
-class IsAuthenticated(BasePermission): 
+
+class IsAuthenticated(BasePermission):
     """
     Base permission to only allow authenticated users.
     """
     @staticmethod
     def has_permission(request: Request, view: 'Type[APIView]') -> bool:
         return request.user and request.user.is_authenticated
+
 
 class IsSuperuser(BasePermission):
     """
@@ -20,3 +24,20 @@ class IsSuperuser(BasePermission):
     @staticmethod
     def has_permission(request: Request, view: 'Type[APIView]') -> bool:
         return request.user and request.user.is_superuser
+
+
+class IsSameAccountOrIsSuperuser(BasePermission):
+    """
+    Custom permission to allow access if the  the resource is connected to the same account as the user 
+    or the user is a superuser.
+    """
+
+    def has_object_permission(self, request: Request, view: 'Type[APIView]', obj: 'Type[models.Model]') -> bool:
+
+        if request.user.is_superuser:
+            return True
+        
+        if isinstance(obj, Account):
+            return request.user.is_authenticated and obj == request.user.account
+
+        return request.user.is_authenticated and hasattr(obj, 'account') and obj.account == request.user.account
