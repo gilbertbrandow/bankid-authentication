2fd2f7a9d81bf4369bfa991ab7e42c642b381c2c
Giblert Brandow <simon@0100.se>
2024-06-10 10:00:24 +0200

    Updated type declarations to avoid circular dependency (#10)

--- backend/authentication/permissions.py
+++ backend/authentication/permissions.py
@@ -1,14 +1,16 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
-from rest_framework.views import APIView
-from typing import Type
+from typing import Type, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from rest_framework.views import APIView
 
 class IsAuthenticated(BasePermission): 
     """
     Base permission to only allow authenticated users.
     """
     @staticmethod
-    def has_permission(request: Request, view: Type[APIView]) -> bool:
+    def has_permission(request: Request, view: 'Type[APIView]') -> bool:
         return request.user and request.user.is_authenticated
 
 class IsSuperuser(BasePermission):
@@ -16,5 +18,5 @@ class IsSuperuser(BasePermission):
     Custom permission to only allow superusers to access the view.
     """
     @staticmethod
-    def has_permission(request: Request, view: Type[APIView]) -> bool:
+    def has_permission(request: Request, view: 'Type[APIView]') -> bool:
         return request.user and request.user.is_superuser
