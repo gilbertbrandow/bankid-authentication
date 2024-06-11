f73e9ec1cc6a908c8244e9241b627f8cb1e8c33c
Giblert Brandow <simon@0100.se>
2024-06-11 23:56:00 +0200

    Implemented custom exception class (#11)

--- backend/authentication/permissions.py
+++ backend/authentication/permissions.py
@@ -1,8 +1,11 @@
 from rest_framework.permissions import BasePermission
+from .exceptions import CustomPermissionDenied
 from rest_framework.request import Request
-from typing import Type, TYPE_CHECKING
 from authentication.models import Account
+from typing import Type, TYPE_CHECKING
 from django.db import models
+from authentication.models import Permission
+
 
 if TYPE_CHECKING:
     from rest_framework.views import APIView
@@ -14,8 +17,9 @@ class IsAuthenticated(BasePermission):
     """
     @staticmethod
     def has_permission(request: Request, view: 'Type[APIView]') -> bool:
-        return request.user and request.user.is_authenticated
-
+        if request.user and request.user.is_authenticated:
+            return True
+        raise CustomPermissionDenied(detail='You must be signed in to perform this action.')
 
 class IsSuperuser(BasePermission):
     """
@@ -23,7 +27,9 @@ class IsSuperuser(BasePermission):
     """
     @staticmethod
     def has_permission(request: Request, view: 'Type[APIView]') -> bool:
-        return request.user and request.user.is_superuser
+        if request.user and request.user.is_superuser:
+            return True
+        raise CustomPermissionDenied(detail='You must have superuser privileges to perform this action.')
 
 
 class IsSameAccountOrIsSuperuser(BasePermission):
@@ -34,25 +40,28 @@ class IsSameAccountOrIsSuperuser(BasePermission):
 
     def has_object_permission(self, request: Request, view: 'Type[APIView]', obj: 'Type[models.Model]') -> bool:
 
-        if request.user.is_superuser:
+        if request.user.is_superuser or (isinstance(obj, Account) and request.user.is_authenticated and obj == request.user.account) or (request.user.is_authenticated and hasattr(obj, 'account') and obj.account == request.user.account):
             return True
         
-        if isinstance(obj, Account):
-            return request.user.is_authenticated and obj == request.user.account
+        raise CustomPermissionDenied(detail='You do not have access to this resource as it is not associated with your account.')
 
-        return request.user.is_authenticated and hasattr(obj, 'account') and obj.account == request.user.account
 
 class HasPermission(BasePermission):
     """
     Custom permission to allow access if the user has the specified permission.
     """
+
     def __init__(self, permission_codename: str) -> None:
         self.permission_codename = permission_codename
 
     def has_permission(self, request: Request, view: 'Type[APIView]') -> bool:
         user = request.user
-        
         if not user or not user.is_authenticated:
             return False
 
-        return any(permission.codename == self.permission_codename for permission in user.permissions)
+        if any(permission.codename == self.permission_codename for permission in user.permissions):
+            return True
+        
+        permission = Permission.objects.get(codename=self.permission_codename)
+
+        raise CustomPermissionDenied(detail=f'Missing permission: "{permission.name}"')
