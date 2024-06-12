2da328840f76ebda0f46235ef88411e96d5d3c4e
Giblert Brandow <simon@0100.se>
2024-06-12 21:27:47 +0200

    Created decorator for specific permission (#12)

--- backend/authentication/decorators.py
+++ backend/authentication/decorators.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.request import Request
+from .permissions import HasPermission
 from .views_base import CustomAPIView
 from typing import Callable, Type
 from rest_framework import status
@@ -16,5 +17,16 @@ def get_and_check_object_permissions(model: Type[models.Model]) -> Callable:
                 return Response(data={'detail': f'{model.__name__} not found.'}, status=status.HTTP_404_NOT_FOUND)
             view.check_object_permissions(request=request, obj=obj)
             return view_func(view, request, obj)
+        return _wrapped_view
+    return decorator
+
+
+def check_permission(permission_codename: str) -> Callable:
+    def decorator(view_func: Callable) -> Callable:
+        @wraps(view_func)
+        def _wrapped_view(view: CustomAPIView, request: Request, *args, **kwargs) -> any:
+            permission_checker = HasPermission(permission_codename=permission_codename)
+            permission_checker.has_permission(request, view)
+            return view_func(view, request, *args, **kwargs)
         return _wrapped_view
     return decorator
