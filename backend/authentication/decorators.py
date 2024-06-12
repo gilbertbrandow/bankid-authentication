253d4de1d2172e2f925538078103df6b80fc2d60
Giblert Brandow <simon@0100.se>
2024-06-12 21:15:06 +0200

    Created decorator for get & check object permission (#12)

---
+++ backend/authentication/decorators.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.request import Request
+from .views_base import CustomAPIView
+from typing import Callable, Type
+from rest_framework import status
+from django.db import models
+from functools import wraps
+
+def get_and_check_object_permissions(model: Type[models.Model]) -> Callable:
+    def decorator(view_func: Callable) -> Callable:
+        @wraps(view_func)
+        def _wrapped_view(view: CustomAPIView, request: Request, pk: int) -> any:
+            try:
+                obj = model.objects.get(pk=pk)
+            except model.DoesNotExist:
+                return Response(data={'detail': f'{model.__name__} not found.'}, status=status.HTTP_404_NOT_FOUND)
+            view.check_object_permissions(request=request, obj=obj)
+            return view_func(view, request, obj)
+        return _wrapped_view
+    return decorator
