66b8609d966256ae7c435d2aa618da81da6ed029
Giblert Brandow <simon@0100.se>
2024-06-16 12:17:55 +0200

    Updated fetching of env variables (#4)

--- backend/project/settings/__init__.py
+++ backend/project/settings/__init__.py
@@ -1,8 +1,8 @@
 import os
 from typing import Any
 
-def get_secret(secret_id: str, backup:Any=None)->(str | None):
-    return os.getenv(secret_id, backup)
+def get_secret(secret_id: str, backup: Any=None)->(str | None):
+    return os.getenv(key=secret_id, default=backup)
 
 if get_secret('PIPELINE') == 'production':
     from .production import *
