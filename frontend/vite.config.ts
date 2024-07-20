8045c6b93b202dd9562591f18d8cbfb7c392cc15
Giblert Brandow <simon@0100.se>
2024-07-20 22:58:39 +0200

    Updated vite config (#4)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
+// vite.config.ts or vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, 
+    port: 5173,
+  },
 })
