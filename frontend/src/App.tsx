67004ac425808041cf2fb27821cd64abcc78bd8a
Giblert Brandow <simon@0100.se>
2024-07-22 12:36:54 +0200

    Made use of sonner for notifications (#20)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,21 @@
 import { Button } from "../src/components/ui/button";
-import { useToast } from "../src/components/ui/use-toast";
-import { Toaster } from "../src/components/ui/toaster";
-import { ToastAction } from "../src/components/ui/toast";
+import { Toaster } from "../src/components/ui/sonner";
+import { toast } from "sonner"
 
 const App = () => {
-  const { toast } = useToast();
 
   return (
     <>
       <Button
         variant="outline"
         onClick={() => {
-          toast({
-            title: "Scheduled: Catch up ",
-            description: "Friday, February 10, 2023 at 5:45 PM",
-            action: (
-              <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
-            ),
-          });
+          toast("Event has been created", {
+            description: "Sunday, December 03, 2023 at 9:00 AM",
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
         }}
       >
         Add to calendar
