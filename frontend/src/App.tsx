9450ba246c4433aa100487f690c00a9c4da2e057
Giblert Brandow <simon@0100.se>
2024-07-22 09:29:40 +0200

    Made use of notification component (#20)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,29 @@
-import React from "react";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "../src/components/ui/accordion";
+import { Button } from "../src/components/ui/button";
+import { useToast } from "../src/components/ui/use-toast";
+import { Toaster } from "../src/components/ui/toaster";
+import { ToastAction } from "../src/components/ui/toast";
 
 const App = () => {
+  const { toast } = useToast();
+
   return (
-    <div style={{ maxWidth: "500px", marginLeft: "100px" }}>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      </Accordion>
-    </div>
+    <>
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast({
+            title: "Scheduled: Catch up ",
+            description: "Friday, February 10, 2023 at 5:45 PM",
+            action: (
+              <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+            ),
+          });
+        }}
+      >
+        Add to calendar
+      </Button>
+      <Toaster />
+    </>
   );
 };
 
