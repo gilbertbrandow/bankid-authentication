dfe82e536b82003fd0a9ecd62e25707c015921d6
Giblert Brandow <simon@0100.se>
2024-07-24 16:06:53 +0200

    Wrapped base layout in theme provider (#23)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provider";
 import LoginLayout from "./components/layouts/Login";
 import EmailLogin from "./pages/Authentication/EmailLogin";
 import RecoverPassword from "./pages/Authentication/RecoverPassword";
@@ -8,17 +9,22 @@ import BankIDSameDeviceLogin from "./pages/Authentication/BankIDSameDeviceLogin"
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="email-login" element={<EmailLogin />} />
-          <Route path="bankid-login" element={<BankIDLogin />} />
-          <Route path="bankid-same-device-login" element={<BankIDSameDeviceLogin />} />
-          <Route path="recover-password" element={<RecoverPassword />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="email-login" element={<EmailLogin />} />
+            <Route path="bankid-login" element={<BankIDLogin />} />
+            <Route
+              path="bankid-same-device-login"
+              element={<BankIDSameDeviceLogin />}
+            />
+            <Route path="recover-password" element={<RecoverPassword />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
