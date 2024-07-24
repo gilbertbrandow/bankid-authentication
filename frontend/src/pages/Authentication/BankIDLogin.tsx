eae6a9838dede729d7d17eb191f9c111f8432607
Giblert Brandow <simon@0100.se>
2024-07-24 16:31:01 +0200

    Inverted qr code based on theme (closes #23)

--- frontend/src/pages/Authentication/BankIDLogin.tsx
+++ frontend/src/pages/Authentication/BankIDLogin.tsx
@@ -1,17 +1,22 @@
 import { Button } from "../../components/ui/button";
 import { useNavigate } from "react-router-dom";
 import BankIDLogo from "../../components/icons/BankIDLogo";
+import { useTheme } from "../../components/theme-provider";
 
 const BankIDLogin = () => {
   const navigate = useNavigate();
+  const { theme } = useTheme();
 
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-3xl font-bold mb-2 text-center inline-flex items-center">Scan (<BankIDLogo size={3.5} color="#000"/>) QR Code</h1>
+      <h1 className="text-3xl font-bold mb-2 text-center inline-flex items-center">
+        Scan (<BankIDLogo size={3.5} color="#000" />) QR Code
+      </h1>
       <div className="max-w-[320px]">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/4/41/QR_Code_Example.svg"
           alt="BankID QR Code"
+          style={{ filter: theme === "dark" ? "invert(1)" : "" }}
         />
       </div>
       <p className="mb-8 mt-4 text-sm text-muted-foreground text-center max-w-[260px]">
