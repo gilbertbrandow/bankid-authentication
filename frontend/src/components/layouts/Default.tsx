00f8a1a30e66cddb4f779ec9a3220a5be75cf490
Giblert Brandow <simon@0100.se>
2024-07-25 23:06:48 +0200

    Added frontend component for logout (#32)

--- frontend/src/components/layouts/Default.tsx
+++ frontend/src/components/layouts/Default.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useLogout } from "../../lib/useLogout";
 
 const DefaultLayout: React.FC = () => {
+  const logout = useLogout();
+
   return (
     <div className="">
       <header className="">Default Header</header>
+      <Button variant="outline" onClick={() => logout()}>Logout</Button>
       <main className="">
         <Outlet />
       </main>
