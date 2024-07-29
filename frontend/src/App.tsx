9771a3199f8b0feb822868b59ca7c2fcabd267be
Giblert Brandow <simon@0100.se>
2024-07-29 22:37:18 +0200

    Added wrapper (#25)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ import Permissions from "./pages/Permissions";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { Suspense } from "react";
-import { Toaster } from "./components/ui/sonner"
+import { Toaster } from "./components/ui/sonner";
 
 const Loading = () => <div>Loading...</div>;
 
@@ -19,26 +19,33 @@ const App = () => {
   return (
     <ThemeProvider defaultTheme="light" storageKey="application-ui-theme">
       <I18nextProvider i18n={i18n}>
-        <Suspense fallback={<Loading />}>
-          <Router>
-            <Routes>
-              <Route element={<LoginLayout />}>
-                <Route path="login" element={<Login />} />
-                <Route path="email-login" element={<EmailLogin />} />
-                <Route path="bankid-login" element={<BankIDLogin />} />
-                <Route
-                  path="bankid-same-device-login"
-                  element={<BankIDSameDeviceLogin />}
-                />
-                <Route path="recover-password" element={<RecoverPassword />} />
-              </Route>
-              <Route element={<DefaultLayout />}>
-                <Route path="permissions" element={<Permissions />} />
-              </Route>
-            </Routes>
-          </Router>
-          <Toaster />
-        </Suspense>
+        <div vaul-drawer-wrapper="">
+          <div className="relative flex min-h-screen flex-col bg-background">
+            <Suspense fallback={<Loading />}>
+              <Router>
+                <Routes>
+                  <Route element={<LoginLayout />}>
+                    <Route path="login" element={<Login />} />
+                    <Route path="email-login" element={<EmailLogin />} />
+                    <Route path="bankid-login" element={<BankIDLogin />} />
+                    <Route
+                      path="bankid-same-device-login"
+                      element={<BankIDSameDeviceLogin />}
+                    />
+                    <Route
+                      path="recover-password"
+                      element={<RecoverPassword />}
+                    />
+                  </Route>
+                  <Route element={<DefaultLayout />}>
+                    <Route path="permissions" element={<Permissions />} />
+                  </Route>
+                </Routes>
+              </Router>
+              <Toaster richColors />
+            </Suspense>
+          </div>
+        </div>
       </I18nextProvider>
     </ThemeProvider>
   );
