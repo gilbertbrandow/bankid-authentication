170e1b072f7a60ab259dfdffdc1e5c94e69bf65c
Giblert Brandow <simon@0100.se>
2024-07-30 14:22:53 +0200

    Added support link & logo link (#30, #35)

--- frontend/src/components/layouts/Login.tsx
+++ frontend/src/components/layouts/Login.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
 import { Outlet } from "react-router-dom";
-import { Github, ArrowLeft } from "lucide-react";
+import { ArrowLeft, LifeBuoy } from "lucide-react";
 import Logo from "../icons/Logo";
 import { Button } from "../ui/button";
 import { ThemeToggle } from "../ui/theme-toggle";
@@ -33,10 +33,16 @@ const LoginLayout: React.FC<LoginLayoutProps> = ({ children }) => {
   return (
     <div className="flex h-screen">
       <div className="relative w-2/5 h-full flex items-center justify-center py-12">
-        <header className="absolute top-0 w-full flex justify-between items-center p-8">
-          <Logo color="--primary" size={32} />
+        <header className="absolute top-0 w-full flex justify-between items-center pr-4 pl-4 pt-4">
+          <Button
+            variant="ghost"
+            className="py-8"
+            onClick={() => navigate("login")}
+          >
+            <Logo color="--primary" size={32} />
+          </Button>
           <div className="flex items-right gap-2">
-            {location.pathname !== "/login" && (
+            {location.pathname !== "/lo gin" && (
               <Button
                 variant="ghost"
                 className="pl-2 pr-2 flex items-center gap-1"
@@ -51,20 +57,20 @@ const LoginLayout: React.FC<LoginLayoutProps> = ({ children }) => {
           </div>
         </header>
         <Outlet />
-        <footer className="absolute bottom-0 w-full text-sm text-muted-foreground p-10">
+        <footer className="absolute bottom-0 w-full text-sm text-muted-foreground pb-8 px-6">
           <hr
-            className="border-1 mb-4"
-            style={{ borderColor: "hsl(var(--input))" }}
+            className="border-1 mb-3"
+            style={{ borderColor: "hsl(var(--border))" }}
           />
-          <div className="flex justify-between">
-            <a
-              target="_blank"
-              href="https://github.com/gilbertbrandow"
-              className="underline flex items-center gap-1"
+          <div className="flex justify-between items-center">
+            <Button
+              variant="link"
+              className="font-normal text-muted-foreground text-sm p-0 mt-0"
+              onClick={() => navigate("login-support")}
             >
-              <Github className="h-4 w-4" />
-              gilbertbrandow
-            </a>
+              <LifeBuoy size={18} className="mr-1" />
+              {t("Support")}
+            </Button>
             <span>
               {" "}
               {t("Copyright")} © {new Date().getFullYear()}{" "}
@@ -82,10 +88,7 @@ const LoginLayout: React.FC<LoginLayoutProps> = ({ children }) => {
         />
         <div className="absolute bottom-8 left-8 bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg text-white pl-2 pr-4 py-2 rounded-lg flex items-center gap-2">
           <Avatar className="h-8 w-8">
-            <AvatarImage
-              src="/yaroslav-shuraev.jpeg"
-              alt="Yaroslav Shuraev"
-            />
+            <AvatarImage src="/yaroslav-shuraev.jpeg" alt="Yaroslav Shuraev" />
             <AvatarFallback>YS</AvatarFallback>
           </Avatar>
           <span>
